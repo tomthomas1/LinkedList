ad5115fed942a3f11b8459414ff4b71dc964082a
tomthomas1 <tomthomas8688@gmail.com>
2022-03-03 23:20:27 +0530

    LinkedList by appending 30 & 70 to 56

--- src/com/bridgelabz/LinkedList.java
+++ src/com/bridgelabz/LinkedList.java
@@ -12,15 +12,15 @@ public class LinkedList {
 		LinkedListServices list = new LinkedListServices(); 
 		/**
 		 * Here we are calling the add method to add node
-		 * We used addFirst method 
-		 *  Node with data 70 is First Created
-		 *  Next 30 is added to 70
-		 *  Finally 56 is added to 30
+		 * We used add method to append 30 and 70 to 56 
+		 *  Node with data 56 is First Created
+		 *  Next Append 30 to 56
+		 *  Finally Append 70 to 30
 		 *  LinkedList Sequence: 56->30->70
 		 */
-		list.addFirst(70);
-		list.addFirst(30);
-		list.addFirst(56);
+		list.add(56);
+		list.add(30);
+		list.add(70);
 		list.printList();
 		
 }
